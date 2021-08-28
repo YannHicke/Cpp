a61e3eac172fb91b2d4f6d4a9c5431d47c311341
Yann Hicke <yann.hicke@gmail.com>
2021-08-28 10:59:27 -0700

    Create initial layout for question a.

---
+++ yh4395_hw8_q2.cpp
@@ -0,0 +1,19 @@
+/* This program will focus on the recursive implementation of
+two problems.
+1. Summing the squares of each value of an array.
+2. Returning a boolean stating whether an array is sorted or not. */
+#include <iostream>
+using namespace std;
+
+int sumOfSquares(int arr[], int arrSize);
+// returns the sum of the squares of each value of the array
+
+int main()
+{
+
+}
+
+int sumOfSquares(int arr[], int arrSize)
+{
+    
+}
