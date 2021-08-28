2bb298b4605192826984353797309231ac297241
Yann Hicke <yann.hicke@gmail.com>
2021-08-28 09:58:19 -0700

    Create initial layout question a.

---
+++ yh4395_hw8_q1.cpp
@@ -0,0 +1,24 @@
+/* This program will focus on recursive implementations of three problems.
+1. Printing a textual image of a right triangle.
+2. Printing a textual image of two opposite right triangles.
+3. Printing a vertical ruler of 2^n - 1 lines. */
+#include <iostream>
+using namespace std;
+
+void printTriangle(int n);
+// prints a right triangle with n lines
+
+int main()
+{
+    int inputUser;
+    cout<<"Question a.\n";
+    cout<<"Please enter a positive integer: ";
+    cin>>inputUser;
+
+    return 0;
+}
+
+void printTriangle(int n)
+{
+
+}
