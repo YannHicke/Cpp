4fd98ef0f42f36504d646491fcb26f4be84b3ffe
Yann Hicke <yann.hicke@gmail.com>
2021-08-28 12:11:42 -0700

    Create initial layout version 1.

---
+++ yh4395_hw8_q3.cpp
@@ -0,0 +1,22 @@
+/* This program will focus on the recursive implementations
+of a function whose goal is to find the minimum of an array.
+We will implement a version that will take the size of the array
+and another version that will take the range of indices to be considered */
+#include <iostream>
+using namespace std;
+
+const int SIZE_ARRAY = 10;
+
+int minArray1(int arr[], int arrSize);
+// Returns the minimum of the array
+
+int main()
+{
+    int arr[SIZE_ARRAY] = {9, -2, 14, 12, 3, 6, 2, 1, -9, 15};
+    
+}
+
+int minArray1(int arr[], int arrSize)
+{
+
+}
